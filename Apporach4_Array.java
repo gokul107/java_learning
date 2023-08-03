9ff404fc5d5fa8e4453a8b9a78a94fb1e657c30c
gokul107 <gokulmalempati@gmail.com>
2023-08-03 22:15:55 +0530

    committing Apporach4 array program to github

---
+++ Apporach4_Array.java
@@ -0,0 +1,13 @@
+class Apporach4_Array
+{
+    public static void main(String[] args) 
+	{
+		char[] carr={'a','b','c'};
+		
+		//reading elements
+		for(char ch:carr)
+		{
+			System.out.print(ch+" ");
+		}
+    }
+}
