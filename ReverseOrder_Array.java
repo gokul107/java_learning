5acf7da61d3fa22281c5528c9a42bc840a98825e
gokul107 <gokulmalempati@gmail.com>
2023-08-03 23:33:59 +0530

    committing ReverseOrder array program to github

---
+++ ReverseOrder_Array.java
@@ -0,0 +1,13 @@
+class ReverseOrder_Array
+{
+    public static void main(String[] args)
+    {
+        int[] arr={1,4,3,8,2,0,9};
+
+        //reading reverse
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
