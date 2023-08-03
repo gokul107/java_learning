6458afff2494fe3cc2f08666afd189ac36782fd7
gokul107 <gokulmalempati@gmail.com>
2023-08-03 22:29:59 +0530

    committing Apporach5 array program to github

---
+++ Apporach5_Array.java
@@ -0,0 +1,13 @@
+class Apporach5_Array
+{
+    public static void main(String[] args) 
+	{
+		String[] sarr={"hi","hello","bye"};
+	
+		//reading elements
+		for(String s:sarr)
+		{
+			System.out.print(s+" ");
+		}
+    }
+}
