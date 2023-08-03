63ed21c3ad1f20a52322f599f3e6bbcc0e028dcf
gokul107 <gokulmalempati@gmail.com>
2023-08-03 22:58:37 +0530

    committing sum array program to github

---
+++ Sum_Array.java
@@ -0,0 +1,16 @@
+class Sum_Array
+{
+    public static void main(String[] args)
+    {
+        int[] arr={1,2,3,4,5,6,7};
+        int sum=0;
+
+        //for each loop
+        for(int i:arr)
+        {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+
+}
