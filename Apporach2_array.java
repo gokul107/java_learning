b77a483564565f457268af9edc51c6305b7abdac
gokul107 <gokulmalempati@gmail.com>
2023-08-03 21:49:19 +0530

    committing Apporach2 array program to github

---
+++ Apporach2_array.java
@@ -0,0 +1,13 @@
+class Apporach2_array
+{
+    public static void main(String[] args)
+    {
+        int[] arr={10,20,30,40};
+
+        //reading elements
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
--- Display_Array.java
+++ Display_Array.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
-class Display_Array
+class Display_array
 {
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of the element :");
+		System.out.println("Enter the size of array :");
 		int size=sc.nextInt();
 
 		int[] arr=new int[size];
-
-		//insreting the Element
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.println("Enter the element :");
-			arr[i]=sc.nextInt();
-		}
-		//reading element
-		for(int i=0;i<arr.length;i++)
-		{
-			System.out.print(arr[i]+" ");
-		}
-
+	
+	//inserting the elements
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println("Enter the Element:");
+		arr[i]=sc.nextInt();
 	}
-		
+	//reading the elements
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.print(arr[i]+" " );
+	}
+
+	
+	}
 
 }
