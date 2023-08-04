c7e4fdc61b7d094c71da0c797c71f89d0e18a889
gokul107 <gokulmalempati@gmail.com>
2023-08-04 22:19:46 +0530

    committing all array program to github

---
+++ array/Smallest_array.java
@@ -0,0 +1,19 @@
+class  Smallest_array
+{
+	public static void main(String[] args) 
+	{
+		int[] arr={5,7,1,3,9,6,8};
+
+		int small=arr[0];
+
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]<small)
+			{
+				small=arr[i];
+			}
+		}
+
+		System.out.println(small);
+	}
+}
