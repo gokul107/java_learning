744b563fc33eb3cc2b39eb13432f318ae1f1b56e
gokul107 <gokulmalempati@gmail.com>
2023-08-04 22:14:00 +0530

    committing all array program to github

---
+++ array/Test.java
@@ -0,0 +1,28 @@
+ class  Test
+{
+    public static void main(String[] args) 
+	{
+		int[] arr={5,9,1,2,7,3};
+
+		//ascending logic
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+
+		//for each loop
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		
+    }
+}
